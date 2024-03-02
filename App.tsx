b8d83817406f1a558dcae7b26b8abbfb1dc31e2e
Bruno Duarte <bruno.duarte314@gmail.com>
2024-03-02 01:27:02 -0300

    font nunito-sans added

--- App.tsx
+++ App.tsx
@@ -5,8 +5,16 @@ import { Routes } from "src/routes";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from "@expo-google-fonts/nunito-sans";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
@@ -14,7 +22,7 @@ export default function App() {
         <SafeAreaView
           style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_100 }}
         >
-          <Routes />
+          {fontsLoaded ? <Routes /> : <ActivityIndicator />}
         </SafeAreaView>
       </ThemeProvider>
     </SafeAreaProvider>
